9401f7827673129a9ff22402bece61100ab23e2b
Roza Poplawska <roza.poplawska@gmail.com>
2022-06-20 19:57:32 +0200

    commit on 'master'

--- cypress/integration/testy_goldap_pl/TC_08_RP.spec.js
+++ cypress/integration/testy_goldap_pl/TC_08_RP.spec.js
@@ -1,10 +1,11 @@
 /// <reference types="cypress" />
+import goldapPage from "../../pageObject/goldapPage";
+const goldapObject = new goldapPage();
 
 describe("goldap 08", () => {
     it("przejscie_na_strone_aktualnosci_z_zakladki_sport", () => {
       cy.wejscie_sport();
-      cy.get('#breadcrumb > :nth-child(2) > a').as('aktualnosci');
-      cy.get('@aktualnosci').click();
+      goldapObject.wybierzWejscieAktualnosci().click();
       cy.url().should("contain", "https://www.goldap.pl/pl/229/0/aktualnosci.html");
     });
   });
--- cypress/pageObject/goldapPage.js
+++ cypress/pageObject/goldapPage.js
@@ -1,16 +1,13 @@
 class goldapPage{
-    _wejscieUrzad = ":nth-child(5) > .show-all-div > .show-all-incat-btn > span";
+    
     _wejscieSzkoly = ":nth-child(17) > .show-all-div > .show-all-incat-btn > span";
-    _wejscieSport = ":nth-child(13) > .show-all-div > .show-all-incat-btn > span";
+    _wejscieAktualnosci = "#breadcrumb > :nth-child(2) > a";
 
-    wybierzWejscieUrzad(){
-        return cy.get(this._wejscieUrzad);
-    }
     wybierzWejscieSzkoly(){
         return cy.get(this._wejscieSzkoly);
     }
-    wybierzWejscieSport(){
-        return cy.get(this._wejscieSport);
+    wybierzWejscieAktualnosci(){
+        return cy.get(this._wejscieAktualnosci);
     }
 }
 export default goldapPage;
